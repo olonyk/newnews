88ecb24e9f23e4506a946e7ed75e19f0e9ad64e8
Olov <olonyk@gmail.com>
2017-11-21 16:46:17 +0100

    Basic functionality in place.

--- src/scripts/commands/scan.py
+++ src/scripts/commands/scan.py
@@ -1,11 +1,12 @@
-from ..support.site_reader import SiteReader
 import json
-from .command import Command
-from pprint import pprint
-from pkg_resources import resource_filename
-from pprint import pprint
-import time
+import sys
 from datetime import datetime
+
+from pkg_resources import resource_filename
+
+from ..support.saver import Saver
+from ..support.site_reader import SiteReader
+from .command import Command
 
 
 class Scan(Command):
@@ -29,31 +30,38 @@ class Scan(Command):
         self.new_articles = 0
 
     def run(self):
-        print("Scan running")
-        # Main loop
-        while self.settings["loop"]:
-            # Initialize the site readers
+        """ This is the entry point of the Scan command. First initialize the saving process and a
+            site reader process for each site in the data.
+        """
+        try:
+            # Start the saving process.
+            saver = Saver(args=self.args, parent=self)
+            saver.start()
+            # Start the site reader processes
             site_readers = []
             for site in self.data["sites"]:
-                if site["name"] == "DN ekonomi":
-                    site_reader = SiteReader(args=self.args,
-                                            site_data=site,
-                                            settings=self.settings,
-                                            company_data=self.data["companies"])
-                    site_reader.start()
-                    site_readers.append(site_reader)
+                site_reader = SiteReader(args=self.args,
+                                         site_data=site,
+                                         settings=self.settings,
+                                         company_data=self.data["companies"],
+                                         saver_queue=saver.queue)
+                site_reader.start()
+                site_readers.append(site_reader)
+        except KeyboardInterrupt:
             for site_reader in site_readers:
+                site_reader.terminate()
                 site_reader.join()
-            self.print_state()
-            self.iterations += 1
-            time.sleep(self.settings["loop interval sec"])
-    
+            saver.queue.put({"post type":"quit"})
+            saver.join()
+            sys.exit(1)
+
     def print_state(self):
         running_time = (datetime.now()-self.start_time)
         days, seconds = running_time.days, running_time.seconds
         hours = days * 24 + seconds // 3600
         minutes = (seconds % 3600) // 60
         seconds = seconds % 60
-        self.log("|¯Iteration:    {}".format(self.iterations))
+        self.iterations += 1
+        self.log("|¯Scans:        {}".format(self.iterations))
         self.log("| Running time: {} {:02d}:{:02d}:{:02d}".format(days, hours, minutes, seconds))
         self.log("|_New articles: {}".format(self.new_articles))
